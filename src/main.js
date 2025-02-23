650974782189c664ffd5df325bba7df1acf20492
Cris John Cañales <crisjohn.canales@gmail.com>
2025-02-23 23:03:23 +0800

    this is the working sakai themed LAMMS

---
+++ src/main.js
@@ -0,0 +1,27 @@
+// src/main.js
+import '@/assets/styles.scss'; // Ensure this path is correct
+import Aura from '@primevue/themes/aura'; // Import the Aura theme
+import 'primeicons/primeicons.css';
+import PrimeVue from 'primevue/config';
+import ConfirmationService from 'primevue/confirmationservice';
+
+import ToastService from 'primevue/toastservice';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+
+app.use(router);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: '.app-dark'
+        }
+    }
+});
+app.use(ToastService);
+app.use(ConfirmationService);
+
+app.mount('#app');
