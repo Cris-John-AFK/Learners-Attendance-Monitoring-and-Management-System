9296c3558ddb2d11aad6bd7a6df35d62500a3e81
Cris John Cañales <crisjohn.canales@gmail.com>
2025-03-21 09:24:35 +0800

    Updated Teacher Settings to have proper settings

--- src/views/pages/Settings.vue
+++ src/views/pages/Settings.vue
@@ -1,83 +1,28 @@
 <script setup>
-import { ProductService } from '@/router/service/ProductService';
-import { useToast } from 'primevue/usetoast';
-import { onMounted, ref } from 'vue';
+import { ref } from 'vue';
 
-onMounted(() => {
-    ProductService.getProducts().then((data) => (products.value = data));
-});
+const sections = ref([
+    { id: '1', section: 'Section A', startTime: '08:00 AM', endTime: '09:00 AM' },
+    { id: '2', section: 'Section B', startTime: '09:15 AM', endTime: '10:15 AM' },
+    { id: '3', section: 'Section C', startTime: '10:30 AM', endTime: '11:30 AM' }
+]);
 
-const toast = useToast();
-const dt = ref();
-const products = ref();
-const productDialog = ref(false);
-const deleteProductDialog = ref(false);
-const deleteProductsDialog = ref(false);
-const product = ref({});
-const selectedProducts = ref();
-const submitted = ref(false);
+const scheduleDialog = ref(false);
+const selectedSchedule = ref(null);
 
-function openNew() {
-    product.value = {};
-    submitted.value = false;
-    productDialog.value = true;
+function editSchedule(section) {
+    selectedSchedule.value = { ...section }; // Clone the section object
+    scheduleDialog.value = true;
 }
 
-function hideDialog() {
-    productDialog.value = false;
-    submitted.value = false;
-}
-
-function saveProduct() {
-    submitted.value = true;
-    if (product?.value.name?.trim()) {
-        if (product.value.id) {
-            products.value[findIndexById(product.value.id)] = product.value;
-            toast.add({ severity: 'success', summary: 'Successful', detail: 'Product Updated', life: 3000 });
-        } else {
-            product.value.id = createId();
-            products.value.push(product.value);
-            toast.add({ severity: 'success', summary: 'Successful', detail: 'Product Created', life: 3000 });
+function saveSchedule() {
+    if (selectedSchedule.value) {
+        const index = sections.value.findIndex(sec => sec.id === selectedSchedule.value.id);
+        if (index !== -1) {
+            sections.value[index] = { ...selectedSchedule.value }; // Update the section
         }
-        productDialog.value = false;
-        product.value = {};
     }
-}
-
-function editProduct(prod) {
-    product.value = { ...prod };
-    productDialog.value = true;
-}
-
-function confirmDeleteProduct(prod) {
-    product.value = prod;
-    deleteProductDialog.value = true;
-}
-
-function deleteProduct() {
-    products.value = products.value.filter((val) => val.id !== product.value.id);
-    deleteProductDialog.value = false;
-    product.value = {};
-    toast.add({ severity: 'success', summary: 'Successful', detail: 'Product Deleted', life: 3000 });
-}
-
-function findIndexById(id) {
-    return products.value.findIndex((val) => val.id === id);
-}
-
-function createId() {
-    return Math.random().toString(36).substr(2, 9);
-}
-
-function confirmDeleteSelected() {
-    deleteProductsDialog.value = true;
-}
-
-function deleteSelectedProducts() {
-    products.value = products.value.filter((val) => !selectedProducts.value.includes(val));
-    deleteProductsDialog.value = false;
-    selectedProducts.value = null;
-    toast.add({ severity: 'success', summary: 'Successful', detail: 'Products Deleted', life: 3000 });
+    scheduleDialog.value = false;
 }
 </script>
 
@@ -86,75 +31,37 @@ function deleteSelectedProducts() {
         <div class="card">
             <Toolbar class="mb-6">
                 <template #start>
-                    <h4 class="m-0">Schedules</h4>
-                </template>
-                <template #end>
-                    <Button label="New" icon="pi pi-plus" severity="secondary" class="mr-2" @click="openNew" />
-                    <Button label="Delete" icon="pi pi-trash" severity="secondary" @click="confirmDeleteSelected" :disabled="!selectedProducts || !selectedProducts.length" />
+                    <h4 class="m-0">Section Schedules</h4>
                 </template>
             </Toolbar>
 
-            <DataTable ref="dt" v-model:selection="selectedProducts" :value="products" dataKey="id" :paginator="true" :rows="10">
-                <Column selectionMode="multiple" style="width: 3rem" :exportable="false"></Column>
-                <Column field="name" header="Name" sortable></Column>
-                <Column field="setTime" header="Set Time" sortable>
+            <DataTable :value="sections" dataKey="id" :paginator="true" :rows="10">
+                <Column field="section" header="Section" sortable></Column>
+                <Column field="startTime" header="Start Time" sortable></Column>
+                <Column field="endTime" header="End Time" sortable></Column>
+                <Column header="Actions">
                     <template #body="slotProps">
-                        {{ slotProps.data.setTime }}
-                    </template>
-                </Column>
-                <Column :exportable="false">
-                    <template #body="slotProps">
-                        <Button icon="pi pi-pencil" outlined rounded class="mr-2" @click="editProduct(slotProps.data)" />
-                        <Button icon="pi pi-trash" outlined rounded severity="danger" @click="confirmDeleteProduct(slotProps.data)" />
+                        <Button icon="pi pi-pencil" outlined rounded class="mr-2" @click="editSchedule(slotProps.data)" />
                     </template>
                 </Column>
             </DataTable>
         </div>
 
-        <Dialog v-model:visible="productDialog" :style="{ width: '450px' }" header="Product Details" :modal="true">
+        <!-- Edit Schedule Modal -->
+        <Dialog v-model:visible="scheduleDialog" :style="{ width: '450px' }" header="Edit Schedule" :modal="true">
             <div class="flex flex-col gap-6">
                 <div>
-                    <label for="name" class="block font-bold mb-3">Name</label>
-                    <InputText id="name" v-model.trim="product.name" required autofocus />
+                    <label class="block font-bold mb-3">Start Time</label>
+                    <Calendar v-model="selectedSchedule.startTime" showIcon iconDisplay="input" timeOnly />
                 </div>
                 <div>
-                    <label for="setTime" class="block font-bold mb-3">Set Time</label>
-                    <Calendar v-model="product.setTime" showIcon iconDisplay="input" timeOnly>
-                        <template #inputicon="{ clickCallback }">
-                            <InputIcon class="pi pi-clock cursor-pointer" @click="clickCallback" />
-                        </template>
-                    </Calendar>
+                    <label class="block font-bold mb-3">End Time</label>
+                    <Calendar v-model="selectedSchedule.endTime" showIcon iconDisplay="input" timeOnly />
                 </div>
             </div>
-
             <template #footer>
-                <Button label="Cancel" icon="pi pi-times" text @click="hideDialog" />
-                <Button label="Save" icon="pi pi-check" @click="saveProduct" />
-            </template>
-        </Dialog>
-
-        <Dialog v-model:visible="deleteProductDialog" :style="{ width: '450px' }" header="Confirm" :modal="true">
-            <div class="flex items-center gap-4">
-                <i class="pi pi-exclamation-triangle !text-3xl" />
-                <span v-if="product"
-                    >Are you sure you want to delete <b>{{ product.name }}</b> with set time <b>{{ product.setTime }}</b
-                    >?</span
-                >
-            </div>
-            <template #footer>
-                <Button label="No" icon="pi pi-times" text @click="deleteProductDialog = false" />
-                <Button label="Yes" icon="pi pi-check" @click="deleteProduct" />
-            </template>
-        </Dialog>
-
-        <Dialog v-model:visible="deleteProductsDialog" :style="{ width: '450px' }" header="Confirm" :modal="true">
-            <div class="flex items-center gap-4">
-                <i class="pi pi-exclamation-triangle !text-3xl" />
-                <span>Are you sure you want to delete the selected schedules?</span>
-            </div>
-            <template #footer>
-                <Button label="No" icon="pi pi-times" text @click="deleteProductsDialog = false" />
-                <Button label="Yes" icon="pi pi-check" @click="deleteSelectedProducts" />
+                <Button label="Cancel" icon="pi pi-times" text @click="scheduleDialog = false" />
+                <Button label="Save" icon="pi pi-check" @click="saveSchedule" />
             </template>
         </Dialog>
     </div>
