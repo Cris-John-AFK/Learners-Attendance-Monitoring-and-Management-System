3a886e12768fbdfb9b5a18dc8f385314a8d20998
Cris John Cañales <crisjohn.canales@gmail.com>
2025-10-14 22:59:04 +0800

    Cleaned useless files

--- lamms-backend/start-server.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Laravel Development Server (Direct PHP)...
-echo Server will be available at: http://127.0.0.1:8000
-echo Press Ctrl+C to stop the server
-echo.
-cd /d "%~dp0"
-php -S 127.0.0.1:8000 -t public
-pause
--- run_migrations.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Running Laravel migrations...
-cd lamms-backend
-php artisan migrate --force
-echo Migrations completed!
-pause
