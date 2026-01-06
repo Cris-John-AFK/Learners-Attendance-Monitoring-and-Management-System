e9744b1b218ba7e21289daa69b233fffe80890e4
crisjohn0627 <crisjohn.canales@msunaawan.edu.ph>
2026-01-06 21:03:45 +0800

    Fix API middleware: Enable Sanctum stateful and remove conflicting CORS

--- lamms-backend/app/Http/Kernel.php
+++ lamms-backend/app/Http/Kernel.php
@@ -37,8 +37,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \App\Http\Middleware\CustomCorsMiddleware::class,
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
